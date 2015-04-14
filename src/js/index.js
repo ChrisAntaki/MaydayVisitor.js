e302b20f1b0ded92639e89146eddd855dcecaa31
Chris Antaki <ChrisAntaki@gmail.com>
2015-04-13 22:22:11 -0700

    Example usage

---
+++ src/js/index.js
@@ -0,0 +1,66 @@
+var MaydayVisitor = require('./MaydayVisitor');
+
+window.visitor = new MaydayVisitor();
+
+window.updateLog = function() {
+    document.querySelector('#log').textContent = JSON.stringify(visitor.info);
+};
+
+window.exampleResetExampleInfo = function() {
+    visitor.addInfo({
+      'uuid': 'abcd-1234-5678-efgh',
+      'first_name': 'joseph',
+      'last_name': 'smith',
+      'is_volunteer': false,
+      'actions': [
+        {
+          'name': 'email_zip',
+          'order': 0,
+          'completed': true,
+          'template_id': 'md_action_email_zip',
+        },
+        {
+          'name': 'research',
+          'order': 1,
+          'completed': false,
+          'template_id': 'md_action_research'
+        }
+      ]
+    });
+
+    updateLog();
+};
+
+window.exampleVolunteer = function() {
+    visitor.addInfo({
+        is_volunteer: true
+    });
+
+    updateLog();
+};
+
+window.exampleCompleteResearchAction = function() {
+    visitor.addAction({
+        'name': 'research',
+        'completed': true,
+        'template_id': 'md_action_research'
+    });
+
+    updateLog();
+};
+
+window.exampleSuggestTemplate = function() {
+    var template = visitor.suggestTemplate();
+
+    alert('Suggested Template:\n' + template);
+
+    updateLog();
+};
+
+window.examplePrefillForm = function() {
+    visitor.populateForm('#example-form');
+};
+
+window.example
+
+updateLog();
